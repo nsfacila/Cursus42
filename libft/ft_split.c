141bad06da83053d839e066d57019457bd28c8f0
Noelia Sanchez Facila <noelsanc@c3r2s2.42malaga.com>
2024-05-17 22:53:09 +0200

    libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: noelsanc <noelsanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:22:34 by noelsanc          #+#    #+#             */
-/*   Updated: 2024/05/17 21:28:41 by noelsanc         ###   ########.fr       */
+/*   Updated: 2024/05/17 22:50:05 by noelsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static int ft_count_words(char const *str, char c)
 	words = 0;
 	while (str[i] != '\0')
 	{
-		if ((str[i] != c && (str[i + 1]) == c) || str[i + 1] == '\0')
+		if (str[i] != c && (str[i + 1] == c || str[i + 1] == '\0'))
 			words ++;
 		i++;
 	}
