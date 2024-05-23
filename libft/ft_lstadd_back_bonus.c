ce461e6b2e8f5d36fc657859926a1b2ac960ce90
Noelia Sanchez facila <noeliasanchezfacila@MacBook-Air-de-Noelia.local>
2024-05-23 17:57:47 +0200

    libft

--- libft/ft_lstadd_back_bonus.c
+++ libft/ft_lstadd_back_bonus.c
@@ -16,5 +16,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-	ft_lstlast(*lst) ->next = new;
+	if (*lst)
+		(ft_lstlast(*lst))-> next = new;
+	else
+		*lst = new;
 }
