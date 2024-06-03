c60caf039191456825b2586168443923377106af
noelia <nsfacila@hotmail.com>
2024-06-03 21:37:12 +0200

    printf

---
+++ Printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: noelsanc <noelsanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 22:40:04 by noelsanc          #+#    #+#             */
+/*   Updated: 2024/06/03 20:42:00 by noelsanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+
+#endif
